123a4a9a970209ff305c200f8948b195fc6df1b4
Darwin <iamdarwin.salcedo@gmail.com>
2023-08-11 09:48:54 -0300

    implemented filters

--- app/src/main/java/com/littlelemon/menu/domain/FilterHelper.kt
+++ app/src/main/java/com/littlelemon/menu/domain/FilterHelper.kt
@@ -1,15 +1,25 @@
 package com.littlelemon.menu.domain
 
 import com.littlelemon.menu.data.ProductItem
+import com.littlelemon.menu.domain.ProductsHelper.DESSERT_CATEGORY
+import com.littlelemon.menu.domain.ProductsHelper.DRINKS_CATEGORY
+import com.littlelemon.menu.domain.ProductsHelper.FOOD_CATEGORY
 
 class FilterHelper {//TODO create a FilterHelperTest and write a unit test for filterProducts
 
     fun filterProducts(type: FilterType, productsList: List<ProductItem>): List<ProductItem> {
         return when (type) {
             FilterType.All -> productsList
-            FilterType.Dessert -> TODO("only products with category equal to Dessert")
-            FilterType.Drinks -> TODO("only products with category equal to Drinks")
-            FilterType.Food -> TODO("only products with category equal to Food")
+
+            FilterType.Dessert ->
+                productsList.filter { it.category == DESSERT_CATEGORY }
+
+
+            FilterType.Drinks ->
+                productsList.filter { it.category == DRINKS_CATEGORY }
+
+            FilterType.Food ->
+                productsList.filter { it.category == FOOD_CATEGORY }
         }
     }
 
